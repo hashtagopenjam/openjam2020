dcdc331ec5e0330e339ebccc095606ccfbfec9e6
Luke Dary <ldary@ldary.rdu.csb>
2020-09-20 22:18:42 -0400

    Initial mechanics

---
+++ src/faller/Faller.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+var velocity = Vector2(100, 0)# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	if len(get_colliding_bodies()) > 0:
+#		print(get_colliding_bodies())
+
--- src/main/Main.gd
+++ src/main/Main.gd
@@ -4,23 +4,47 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var score = 0
+var velocity = Vector2(0, 10)
+onready var Blower = preload("res://src/blower/Blower.tscn")
+onready var Faller = preload("res://src/faller/Faller.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	addFaller()
 
+func addFaller():
+	var faller = Faller.instance()
+	faller.global_position = $Start.global_position
+	#faller.linear_velocity = Vector2(100,10)
+	add_child(faller)
+	faller.apply_impulse(Vector2(0,0), Vector2(200,-10))
 
+func addPuff(pos:Vector2):
+	var puff = Blower.instance()
+	puff.position = Vector2(pos.x, pos.y)
+	add_child(puff)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+func _process(delta):
+	$UI.points = score
 #	pass
+	#var collision = $Blower.move_and_collide(velocity * delta)
 
 
 func _on_TapArea_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
-			$Blower.position = Vector2(event.position.x, 1400)
-			$Blower.move_and_collide(Vector2(0,9.8))
+			#print("Clicked: ",event.position)
+			addPuff(event.position)
 	if event is InputEventScreenTouch:
-		$Blower.position = Vector2(event.position.x, 1400)
-		$Blower.move_and_collide(Vector2(0,-10))
+		#print("Tapped: ", event.position)
+		addPuff(event.position)
+
+
+func _on_SpawnTimer_timeout():
+	addFaller()
+
+
+func _on_Gate_body_entered(body):
+	score += 10
+	body.get_parent().remove_child(body)
